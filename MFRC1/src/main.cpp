b70358ef51aca338aec5e4f505ee9f82e908f48c
Yair Shur <yairs@pentenetworks.com>
2025-12-08 21:15:53 +0200

    Update

--- MFRC1/src/main.cpp
+++ MFRC1/src/main.cpp
@@ -40,13 +40,16 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define RST_PIN         9          // Configurable, see typical pin layout above
-#define SS_PIN          10         // Configurable, see typical pin layout above
+#define RST_PIN         25         // Configurable, see typical pin layout above
+#define SS_PIN          27         // Configurable, see typical pin layout above
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
 void setup() {
-	Serial.begin(9600);		// Initialize serial communications with the PC
+//   Serial.begin(115200);// Initialize serial communications with the PC
+//   delay(1000);
+
+	Serial.begin(115200);		
 	while (!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
 	SPI.begin();			// Init SPI bus
 	mfrc522.PCD_Init();		// Init MFRC522
